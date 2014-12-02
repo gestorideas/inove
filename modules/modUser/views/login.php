455d317fc836ed6fcd66089c1f33801df21158de
emil <emilio.agonzalez@hushmail.com>
2014-12-02 16:26:34 +0100

    Modificaciones de estilo ruta de css y bootstrap en login.php

--- modules/modUser/views/login.php
+++ modules/modUser/views/login.php
@@ -18,15 +18,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link href="../../../../login/css/style.css" rel="stylesheet">
-    <link href="../../../../login/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../../css/style.css" rel="stylesheet">
+    <link href="../../../css/bootstrap.min.css" rel="stylesheet">
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
     <script src="../../../js/html5shiv.js"></script>
     <![endif]-->
-    <script type="text/javascript" src="../../../../login/js/jquery.min.js"></script>
-    <script type="text/javascript" src="../../../../login/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../../../../login/js/jqBootstrapValidation.js"></script>
+    <script type="text/javascript" src="../../../js/jquery.min.js"></script>
+    <script type="text/javascript" src="../../../js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../../../js/jqBootstrapValidation.js"></script>
 </head>
 <body>
 <div class="container" style="margin-top:10%">
